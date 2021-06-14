26cd0d7883d0c8dffbce73e3215dcfd81d51d1fa
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-14 13:40:37 +0700

    using flex to design layout

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,14 +1,45 @@
 import React from 'react'
-import { View, Text, ImageBackground } from 'react-native'
+import { 
+  View,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Button,
+  Alert,
+  StyleSheet,
+  ViewStyle,
+  TextInputProps,
+ } from 'react-native'
 
 interface HomeScreenProps {}
 
 const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
     return (
         <ImageBackground source={require('./assets/img/bg.png')} style={{flex:1}}>
-            <Text>Home</Text>
-        </ImageBackground>
+            <View 
+            style={{
+                flexDirection: 'column',
+                height: 200, 
+                backgroundColor: '#FFF7', 
+                marginTop: 30,
+                marginLeft: 30,
+                marginRight: 30,
+                borderRadius: 10,
+                padding: 16,
+                }}>
+                {/* Username Input */}
+                <View style={{flexDirection: 'row'}}>
+                    <View style={{
+                        width: 30, 
+                        height: 30, 
+                        backgroundColor: 'green', 
+                        borderRadius: 15
+                        }}>
 
+                    </View>
+                </View>
+            </View>
+        </ImageBackground>
     )
 }
 
