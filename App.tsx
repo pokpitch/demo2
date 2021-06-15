cb05843251b95b46700c1648e5aa4a9bdcb0063a
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-15 09:55:36 +0700

    using app navigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
  import React from 'react';
- //import HomeScreen from './src/HomeScreen' 
- import RegisterScreen from './src/RegisterScreen'
+ import {View, SafeAreaView, StatusBar} from 'react-native';
+ import AppNavigator from './src/AppNavigator';
+ import {NavigationContainer} from '@react-navigation/native';
+
+
  interface AppProps {}
 
  const App: React.FunctionComponent<AppProps> = props => {
-   return <RegisterScreen/>
+  return (
+    <NavigationContainer>
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView />
+        <AppNavigator />
+      </View>
+    </NavigationContainer>
+  )
  }
 
  export default App;
---
+++ src/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './HomeScreen';
+import RegisterScreen from './RegisterScreen';
+import JSONFeedScreen from './JSONFeedScreen';
+import CameraScreen from './CameraScreen';
+
+const Stack = createStackNavigator();
+
+const RootStack = () => {
+    return (
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='Register' component={RegisterScreen} />
+        </Stack.Navigator>
+    )
+}
+
+export default RootStack;
