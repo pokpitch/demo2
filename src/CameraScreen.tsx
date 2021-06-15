d7379b03068875542b031086954c6dd49b8f63a5
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-15 11:26:57 +0700

    added: camera blank screeneen

---
+++ src/CameraScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+interface CameraScreenProps {}
+
+const CameraScreen: React.FunctionComponent<CameraScreenProps> = props => {
+  return <View />;
+};
+
+export default CameraScreen;
