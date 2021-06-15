1dcef3865a93dd292547f014f21c732c193e7ac2
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-15 10:38:19 +0700

    added: onPress event on cancel button, press to navigate to home screen

--- src/RegisterScreen.tsx
+++ src/RegisterScreen.tsx
@@ -16,8 +16,13 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 import { AccountType } from './types/account.interfeace';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from './RootNavigationParams';
+import {useNavigation} from '@react-navigation/native';
 
 interface RegisterScreenProps {}
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Register'>
 
 const RegisterScreen: React.FunctionComponent<RegisterScreenProps> = (props) => {
 
@@ -26,64 +31,103 @@ const RegisterScreen: React.FunctionComponent<RegisterScreenProps> = (props) => 
         password: ''
     });
 
-    return (
-        <ImageBackground source={require('./assets/img/bg.png')} style={{flex:1}}>
-            {/* Authen Section */}
-            <View 
-            style={{
-                flexDirection: 'column',
-                backgroundColor: '#FFF7', 
-                marginTop: 30,
-                marginLeft: 30,
-                marginRight: 30,
-                borderRadius: 10,
-                padding: 16,
-                }}>
+    const navigation = useNavigation<HomeScreenNavigationProp>();
 
-                {/* Username Input */}
-                <StreamEntry 
-                    icon='user' 
-                    hint='Username'
-                    onChanged={text => {
-                        setAccount({...account, username: text});
-                        console.log('Username: ' + account.username);
-                    }}
-                />
+    // Listen component did mount
+    React.useEffect(() => {
+        setNavigationOption();
+    }, []);
 
-                {/* Password Input */}
-                <StreamEntry 
-                    icon='lock' 
-                    hint='Password' 
-                    isPassword={true}
-                    onChanged={text => {
-                        setAccount({...account, password: text});
-                        console.log('Password: ' + account.password);
-                    }}
-                />
-
-                <Text>Debug: {JSON.stringify(account)}</Text>
-
-                <View style={{marginTop: 30}} />
-                <Button title="Regiter" onPress={() => {
-                    Alert.alert('Register clicked' + JSON.stringify(account))
-                }} />
-
-                <View style={{marginTop: 16}} />
-                <TouchableOpacity style={{height: 30}}>
-                    <Text style={{textAlign: 'center'}}>Cancel</Text>
-                </TouchableOpacity>
-
-            </View>
-
-            {/* Banner */}
-            <Image
-            resizeMode='contain' 
-            source={require('./assets/img/header_react_native.png')} 
-            style={{width: '100%'}}
+    // Initial navigation option
+    const setNavigationOption = () => {
+        navigation.setOptions({
+        title: 'Home',
+        headerStyle: {
+            backgroundColor: '#119CED',
+        },
+        headerTintColor: '#FFFFFF',
+        headerTitleStyle: {color: '#fff'},
+        headerBackTitle: ' ',
+        headerRight: () => (
+            <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => Alert.alert('www.codemobiles.com')}
+            style={{padding: 10}}>
+            <Icon
+                name="address-card"
+                size={20}
+                color="#fff"
+                style={{
+                height: 24,
+                width: 24,
+                }}
             />
-        </ImageBackground>
-    )
-}
+            </TouchableOpacity>
+        ),
+        });
+    };
+        
+  return (
+    <ImageBackground source={require('./assets/img/bg.png')} style={{flex: 1}}>
+      {/* authen section */}
+      <View
+        style={{
+          flexDirection: 'column',
+          backgroundColor: '#FFF7',
+          marginTop: 30,
+          marginLeft: 30,
+          marginRight: 30,
+          borderRadius: 10,
+          padding: 16,
+        }}>
+        {/* Username Input */}
+        <StreamEntry
+          icon="user"
+          hint="Username"
+          onChanged={text => {
+            setAccount({...account, username: text});
+            console.log('Username: ' + account.username);
+          }}
+        />
+
+        {/* Password Input */}
+        <StreamEntry
+          isPassword={true}
+          icon="lock"
+          hint="Password"
+          onChanged={text => {
+            setAccount({...account, password: text});
+            console.log('Password: ' + account.password);
+          }}
+        />
+
+        <Text>Debug: {JSON.stringify(account)}</Text>
+
+        <View style={{marginTop: 30}} />
+        <Button
+          title="Register"
+          onPress={() => {
+            Alert.alert(JSON.stringify(account));
+          }}
+        />
+
+        <View style={{marginTop: 16}} />
+        <TouchableOpacity
+          style={{height: 30}}
+          onPress={() => navigation.goBack()}>
+          <Text style={{textAlign: 'center'}}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Banner */}
+      <Image
+        resizeMode="contain"
+        source={require('./assets/img/header_react_native.png')}
+        style={{width: '100%'}}
+      />
+    </ImageBackground>
+  );
+}; 
 
 export default RegisterScreen
 
