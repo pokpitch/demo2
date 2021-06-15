870d64510548acb5a21eb8579350be5a959d9791
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2021-06-15 11:47:13 +0700

    added : youtube interface

---
+++ src/types/youtube.interface.ts
@@ -0,0 +1,13 @@
+export interface Welcome {
+    youtubes:  Youtube[];
+    error:     boolean;
+    error_msg: string;
+}
+
+export interface Youtube {
+    id:            string;
+    title:         string;
+    subtitle:      string;
+    avatar_image:  string;
+    youtube_image: string;
+}
